db2abe7dd05c434090017f8263413d3a7b80982d
Javier <jmartinezcampos98@gmail.com>
2021-02-10 09:30:11 +0100

    Páginas decoradas con CSS y uso de html básico

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -15,7 +15,7 @@ class QuestionController extends AbstractController
      */
     public function homepage()
     {
-        return new Response('What a bewitching controller we have conjured!');
+        return $this->render('question/homepage.html.twig');
     }
 
     /**
@@ -28,6 +28,8 @@ class QuestionController extends AbstractController
             'Honestly, I like furry shoes better',
             'Maybe try saying the spell backwards',
         ];
+
+        dump($this);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $any_word)),
